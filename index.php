ab34d4614d189e6ef808ba8f055164a7868c5da4
Rakulan SIvathasan <s.rakulan04@gmail.com>
2024-03-18 11:38:38 +0100

    update

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="picture/logo.png">
+    <title>Acceuil - PCS</title>
+</head>
+<body>
+
+    <header></header>
+
+    <script src="js/script.js"></script>
+
+</body>
+</html>
